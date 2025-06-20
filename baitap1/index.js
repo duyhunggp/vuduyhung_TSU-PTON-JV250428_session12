bb73bd6d311436e81abfa23321f0a8b4f33f893e
duyhunggp <vuduyhunggp.1998@gmail.com>
2025-06-20 17:06:44 +0900

    upload file

---
+++ baitap1/index.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("moi ban nhap so a: "));
+let b = Number(prompt("moi ban nhap so b: "));
+if (a % b === 0) {
+  alert("a chia het cho b");
+} else {
+  alert("a khong chia het cho b");
+}
